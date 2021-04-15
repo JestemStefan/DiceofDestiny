5a97f325cec046f737d9af92ed21b10cc1b387fa
JestemStefan <37214990+JestemStefan@users.noreply.github.com>
2021-04-15 16:16:33 +0200

    force push

--- scenes/InteractionBox/Action_Box.gd
+++ scenes/InteractionBox/Action_Box.gd
@@ -8,6 +8,7 @@ var ActionBox_Type: int = Action_type.ATTACK
 onready var tween_size: Tween = $Tween_Size
 signal actionbox_triggered
 
+var assigned_dice = null
 
 func _ready():
 	yield(get_tree(), "idle_frame")
@@ -22,7 +23,6 @@ func get_actionbox_type():
 
 
 func set_actionbox_type(type: int):
-	print(type)
 	ActionBox_Type = type
 	update_actionbox()
 
@@ -30,9 +30,14 @@ func set_actionbox_type(type: int):
 func update_actionbox():
 	match ActionBox_Type:
 		
-		Action_type.ATTACK: $Box_Sprite.frame = 2
-		Action_type.BLOCK: $Box_Sprite.frame = 0
-		Action_type.HEAL: $Box_Sprite.frame = 1
+		Action_type.ATTACK: 
+			$Box_Sprite.frame = 2
+			
+		Action_type.BLOCK: 
+			$Box_Sprite.frame = 0
+			
+		Action_type.HEAL: 
+			$Box_Sprite.frame = 1
 
 
 func use_dice(dice_value: int):
@@ -53,13 +58,59 @@ func use_dice(dice_value: int):
 	emit_signal("actionbox_triggered", action_name, dice_value)
 
 
-func _on_Action_Box_mouse_entered():
-	GameController.current_encounter.picked_action = self
-	var _err_tween = tween_size.interpolate_property(self, "scale", Vector2.ONE, Vector2(1.2, 1.2), 0.1, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
-	var _tween_start = tween_size.start()
+func assign_dice(dice):
+	
+	if assigned_dice != null:
+		assigned_dice.dice.last_position = dice.initial_position
+		assigned_dice.enter_state(dice.State.DRAG)
+		
+		
+	
+	else:
+		assigned_dice = dice
+		$UseButton.disabled = false
 
+
+func release_dice():
+	print("dice released")
+	assigned_dice = null
+	$UseButton.disabled = true
+
+
+func _on_Action_Box_mouse_entered():
+	var _err_tween = tween_size.interpolate_property($Box_Sprite, "scale", null, Vector2(2.2, 2.2), 0.1, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	var _tween_start = tween_size.start()
+	
+	if GameController.current_encounter.current_turn == GameController.current_encounter.Turn.PLAYER:
+		GameController.current_encounter.picked_action = self
+	
 
 func _on_Action_Box_mouse_exited():
-	GameController.current_encounter.picked_action = null
-	var _err_tween = tween_size.interpolate_property(self, "scale", null, Vector2.ONE, 0.1, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	var _err_tween = tween_size.interpolate_property($Box_Sprite, "scale", null, Vector2(2, 2), 0.1, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 	var _tween_start = tween_size.start()
+	
+	
+	if GameController.current_encounter.picked_action == self and GameController.current_encounter.current_turn == GameController.current_encounter.Turn.PLAYER:
+		GameController.current_encounter.picked_action = null
+		
+
+
+
+func _on_UseButton_button_up():
+	
+	var action_name: String
+	
+	match ActionBox_Type:
+		
+		Action_type.ATTACK:
+			action_name = "Attack"
+		
+		Action_type.BLOCK:
+			action_name = "Block"
+		
+		Action_type.HEAL:
+			action_name = "Heal"
+	
+	$UseButton.disabled = true
+	emit_signal("actionbox_triggered", action_name, assigned_dice.get_dice_value())
+	assigned_dice.enter_state(assigned_dice.State.USED)
