f0414e6937ed6caf408338546cd5d4a63d9cf10b
JestemStefan <37214990+JestemStefan@users.noreply.github.com>
2021-04-17 20:49:59 +0200

    New boardmap

--- src/Global/GameController.gd
+++ src/Global/GameController.gd
@@ -1,7 +1,7 @@
 extends Node
 
-enum GameState{BOARD_WAITING, BOARD_TRAVELING, ENCOUNTER}
-var current_game_state: int = GameState.BOARD_WAITING
+enum Game_State{BOARD_WAITING, BOARD_TRAVELING, ENCOUNTER}
+var current_game_state: int = Game_State.BOARD_WAITING
 
 var player: Node2D
 
@@ -26,16 +26,16 @@ func enter_state(new_state):
 	current_game_state = new_state
 	
 	match current_game_state:
-		GameState.BOARD_WAITING:
+		Game_State.BOARD_WAITING:
 			current_board.cover(false)
 			
 			if current_encounter != null:
 				current_encounter.hide()
 				
-		GameState.BOARD_TRAVELING:
+		Game_State.BOARD_TRAVELING:
 			pass
 		
-		GameState.ENCOUNTER:
+		Game_State.ENCOUNTER:
 			current_board.cover(true)
 			current_encounter.show()
 			
@@ -45,15 +45,14 @@ func update_open_tiles(tile_list: Array):
 	open_tiles = tile_list
 
 
+
 func close_opened_tiles():
 	for tile_to_close in open_tiles:
 		tile_to_close.close_tile()
 
 
 func move_to_tile(tile: BoardTile):
-	enter_state(GameState.BOARD_TRAVELING)
-	
-	#print("Trying to move to tile: " + str(tile.get_name()))
+	enter_state(Game_State.BOARD_TRAVELING)
 	
 	var tween = Tween.new()
 	
@@ -75,7 +74,7 @@ func move_to_tile(tile: BoardTile):
 	tile.open_connected_tiles()
 	tile.set_as_current_tile()
 	
-	enter_state(GameState.BOARD_WAITING)
+	enter_state(Game_State.BOARD_WAITING)
 
 
 func start_encounter(encounter_type: String, enemy_data: Resource):
@@ -92,7 +91,10 @@ func start_encounter(encounter_type: String, enemy_data: Resource):
 			encounter.enemy_stats = enemy_data
 			encounter.start_encounter()
 	
-	enter_state(GameState.ENCOUNTER)
+			enter_state(Game_State.ENCOUNTER)
+			
+		"Rest":
+			GameState.player_health = GameState.player_max_health
 
 	transition_tween = transition_layer.make_transition_black_out()
 	yield(transition_tween, "tween_completed")
@@ -100,17 +102,20 @@ func start_encounter(encounter_type: String, enemy_data: Resource):
 
 
 func end_encounter():
+	# Transition to black
 	var transition_tween: Tween = transition_layer.make_transition_black_in()
 	yield(transition_tween, "tween_completed")
 	
-	
+	# Delete encounter
 	current_encounter.call_deferred("free")
 	current_encounter = null
 	
-	enter_state(GameState.BOARD_WAITING)
+	# Prepare the board
+	enter_state(Game_State.BOARD_WAITING)
 	current_board_tile.update_tile_type(current_board_tile.TileTypes.EMPTY_TILE)
 	current_board_tile.enter_state(current_board_tile.TileState.CURRENT)
 	
+	# Transition out of black
 	transition_tween = transition_layer.make_transition_black_out()
 	yield(transition_tween, "tween_completed")
 	transition_layer.transition_black_hide()
