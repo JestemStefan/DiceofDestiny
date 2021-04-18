88b8dbe9d0f18849fbdd3a761161145532c0d95b
John Gabriel <contact@johngabriel.co.uk>
2021-04-18 16:41:48 +0100

    Add Butler bits

---
+++ do_butler_stuff.bat
@@ -0,0 +1,5 @@
+butler push dist\Windows jestemstefan/dice-of-destiny:windows --userversion 1.0.0
+butler push dist\macOS\DiceOfDestiny.zip jestemstefan/dice-of-destiny:mac --userversion 1.0.0
+butler push dist\Linux jestemstefan/dice-of-destiny:linux --userversion 1.0.0
+butler push dist\HTML\HTML.zip jestemstefan/dice-of-destiny:html --userversion 1.0.0
+pause
