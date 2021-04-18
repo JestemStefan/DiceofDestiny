6418eb2c584107443c283fee804a4ef1522017f5
JestemStefan <37214990+JestemStefan@users.noreply.github.com>
2021-04-18 20:30:47 +0200

    Added hints

--- scenes/InteractionBox/Action_Box.gd
+++ scenes/InteractionBox/Action_Box.gd
@@ -6,6 +6,8 @@ enum Action_type{ATTACK, BLOCK, HEAL, SEVEN, SPECIAL_OFF, SPECIAL_ON}
 var ActionBox_Type: int = Action_type.ATTACK
 
 onready var tween_size: Tween = $Tween_Size
+onready var hint_label: Label = $Hint
+
 signal actionbox_triggered
 
 var assigned_dice = null
@@ -33,34 +35,44 @@ func update_actionbox(isPlayer: bool):
 			
 			Action_type.ATTACK: 
 				$Box_Sprite.frame = 0
+				hint_label.text = "Deal X amount of\n damage to enemy"
 				
 			Action_type.BLOCK: 
 				$Box_Sprite.frame = 1
+				hint_label.text = "Block X amount of\n damage in next round"
 				
 			Action_type.HEAL: 
 				$Box_Sprite.frame = 2
+				hint_label.text = "Heal X amount of\n health"
 				
 			Action_type.SEVEN:
 				$Box_Sprite.frame = 3
+				hint_label.text = "Put dice of value 7\n to make skills twice as effective"
 	
 	else:
 		match ActionBox_Type:
 			
 			Action_type.ATTACK: 
 				$Box_Sprite.frame = 4
+				hint_label.text = "Deal X amount of\n damage to player"
 				
 			Action_type.BLOCK: 
 				$Box_Sprite.frame = 5
+				hint_label.text = "Block X amount of\n damage in next round"
 
 				
 			Action_type.HEAL: 
 				$Box_Sprite.frame = 6
+				hint_label.text = "Enemy heal X amount of\n health"
+				
 			
 			Action_type.SPECIAL_OFF: 
 				$Box_Sprite.frame = 7
+				hint_label.text = "Enemy skills twice\n as effective in X amount of rounds"
 				
 			Action_type.SPECIAL_ON: 
 				$Box_Sprite.frame = 8
+				hint_label.text = "Enemy skills twice\n as effective in this round"
 
 
 func use_dice(dice_value: int):
@@ -95,6 +107,8 @@ func _on_Action_Box_mouse_entered():
 	var _err_tween = tween_size.interpolate_property($Box_Sprite, "scale", null, Vector2(1.9, 1.9), 0.1, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 	var _tween_start = tween_size.start()
 	
+	hint_label.show()
+	
 	if GameController.current_encounter.current_turn == GameController.current_encounter.Turn.PLAYER:
 		GameController.current_encounter.picked_action = self
 	
@@ -103,6 +117,7 @@ func _on_Action_Box_mouse_exited():
 	var _err_tween = tween_size.interpolate_property($Box_Sprite, "scale", null, Vector2(2, 2), 0.1, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 	var _tween_start = tween_size.start()
 	
+	hint_label.hide()
 	
 	if GameController.current_encounter.picked_action == self and GameController.current_encounter.current_turn == GameController.current_encounter.Turn.PLAYER:
 		GameController.current_encounter.picked_action = null
